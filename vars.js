fc914216bfb11e0d5f474a8a21fe328ab3929460
Abel Meneses <abelma1980@gmail.com>
2019-08-02 17:10:35 -0400

    Chapter 2: Variables

---
+++ vars.js
@@ -0,0 +1,11 @@
+var favoriteFruits = ["Orange","Melon","Mango","Guava"];
+var carsInParkingLot = ["Toyota","Ferrari","Lexus"];
+var employees = ["Ailan","Leonel","Abel"];
+var primeNumbers = [2,3,5,7,11,13,17,19];
+var randomVariables = [2,"any type", undefined, null, true, 2.51];
+
+elementNumber = 1;
+
+window.onload = function () {};
+
+window.alert(favoriteFruits[elementNumber]);
